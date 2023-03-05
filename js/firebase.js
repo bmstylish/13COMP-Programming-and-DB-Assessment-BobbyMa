a36956ecf053651e372f2810c65612fba66b7bc7
Bobby Ma <54528406+bmstylish@users.noreply.github.com>
2023-03-05 00:04:27 +0000

    changed db into 13comp starting development of new game

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,23 @@
-/************************************************************/ 
+/************************************************************/
 // Written by Bobby Ma Term 1 - 2 2022: Game & Firebase Database 
 // Connecting website to backend firebase realtime database 
 // v01: Copy and paste all essential infomation from realtime firebase database 
 // to this document(firebase.js)
-/************************************************************/ 
+/************************************************************/
 
 var app_firebase = {};
-(function(){
+(function() {
     const firebaseConfig = {
-      apiKey: "AIzaSyDMtiSbtW6tbfKKmqFQ3R3GVfyUolIfjpk",
-      authDomain: "comp-2022-bobby.firebaseapp.com",
-      databaseURL: "https://comp-2022-bobby-default-rtdb.firebaseio.com",
-      projectId: "comp-2022-bobby",
-      storageBucket: "comp-2022-bobby.appspot.com",
-      messagingSenderId: "419493940660",
-      appId: "1:419493940660:web:a6c1dffaf142219885b6e1",
-      measurementId: "G-JDRL0M7M15"
-      };
-    
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-      app_firebase = firebase;
+        apiKey: "AIzaSyAac5JUWkgbGFFg0eoueSHqjCk99zAdsoM",
+        authDomain: "comp-cd621.firebaseapp.com",
+        projectId: "comp-cd621",
+        storageBucket: "comp-cd621.appspot.com",
+        messagingSenderId: "894172637064",
+        appId: "1:894172637064:web:7e5cf88aee0bb94e0bbc3d",
+        measurementId: "G-XKSS949QDC"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    app_firebase = firebase;
 })()
